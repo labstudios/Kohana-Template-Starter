e4b5d7f16cfcc683ceb21302b96f352cb4283e2f
Brent <labstudios@gmail.com>
2013-08-28 04:35:39 +0000

    a more secure setup

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -1,7 +1,5 @@
-            <ul>
-				<li><a href="#" >Home</a></li>
-				<li><a href="#" >Link 1</a></li>
-				<li><a href="#" >Link 2</a></li>
-                <li><a href="#" >Link 3</a></li>
-                <li><a href="#" >Link 4</a></li>
-			</ul>
+<a href="#" >Home</a>
+<a href="#" >Link 1</a>
+<a href="#" >Link 2</a>
+<a href="#" >Link 3</a>
+<a href="#" >Link 4</a>
